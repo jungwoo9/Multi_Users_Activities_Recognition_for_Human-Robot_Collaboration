a62444eb211168456e50da66a01d25047925179a
Jungwoo Koo <jungwoo.koo@student.manchester.ac.uk>
2024-03-04 09:20:10 +0000

    add small loss option in early stopping

--- engine.py
+++ engine.py
@@ -32,6 +32,8 @@ class EarlyStopper:
         self.min_validation_loss = float('inf')
 
     def early_stop(self, validation_loss):
+        if validation_loss < 0.1:
+            return True
         if validation_loss < self.min_validation_loss:
             self.min_validation_loss = validation_loss
             self.counter = 0
